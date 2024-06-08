90c012a723d4e7d4dce3bbd3d98a2d0fa46bb478
Thabang Mposula <78591599+iarxii@users.noreply.github.com>
2024-06-08 15:17:36 +0200

    import starter images, create components from themetemplate file, add sample jobs.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 // import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListing from './components/JobListing'
+import ViewAllJobs from './components/ViewAllJobs'
 
 const App = () => {
   return (
-    <div className='text-5xl'>App</div>
+    <>
+      <body>
+        {/* <!-- Navbar --> */}
+        <Navbar />
+
+        {/* <!-- Hero --> */}
+        <Hero title='🔥Ignite your Future!' subtitle='Where Ambition Meets Opportunity' />
+
+        {/* <!-- Home Cards --> */}
+        <HomeCards />
+
+        {/* <!-- Browse Jobs --> */}
+        <JobListing />
+
+        {/* view all jobs */}
+        <ViewAllJobs />
+      </body>
+    </>
   )
 }
 
